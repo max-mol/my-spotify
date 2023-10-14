529434a517af1f667c90800d7fb9edbbb4f045ad
max-mol <maxime-molines@hotmail.fr>
2023-10-14 08:21:15 +0200

    implement spotify auth0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <div>Home</div>
-}
+'use client';
+
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Box from '@mui/material/Box';
+
+const Home = () => {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <Box>Loading...</Box>;
+  if (error) return <Box>{error.message}</Box>;
+
+  if (user) {
+    return (
+      <Box>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </Box>
+    );
+  }
+
+  return <a href="/api/auth/login">Login</a>;
+};
+
+export default Home;
