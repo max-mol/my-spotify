fddd0fcc407e1fbe047e396070f36eb6922b3480
max-mol <maxime-molines@hotmail.fr>
2023-10-14 10:44:15 +0200

    implement loader while connecting + first home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,51 @@
-'use client';
+"use client";
 
-import { useUser } from '@auth0/nextjs-auth0/client';
-import Box from '@mui/material/Box';
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { CircularProgress } from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
 
 const Home = () => {
   const { user, error, isLoading } = useUser();
 
-  if (isLoading) return <Box>Loading...</Box>;
-  if (error) return <Box>{error.message}</Box>;
-
-  if (user) {
+  if (isLoading)
     return (
-      <Box>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+      <Box textAlign="center" p="200px">
+        <CircularProgress sx={{ color: "#512da8" }} />
       </Box>
     );
-  }
+  if (error) return <Box>{error.message}</Box>;
+
+  return (
+    <Paper
+      sx={{
+        backgroundColor: "#212121",
+        color: "white",
+        p: "150px",
+        m: "40px 150px 40px 150px",
+        textAlign: "center"
+      }}
+    >
+      <Box>Welcome to your Spotify disc collection!</Box>
+      {!user && (
+        <Box>
+          <Box>Please sign in.</Box>
+          <Button
+            href="/api/auth/login"
+            sx={{
+              backgroundColor: "#512da8",
+              color: "white",
+              fontWeight: 700,
+              mt: 3
+            }}
+          >
+            Login to your Spotify account
+          </Button>
+        </Box>
+      )}
+    </Paper>
+  );
 };
 
 export default Home;
--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,22 +1,22 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
+import { useState } from "react";
 
-import Link from 'next/link';
+import Link from "next/link";
 
-import { useUser } from '@auth0/nextjs-auth0/client';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import MenuIcon from '@mui/icons-material/Menu';
-import AlbumIcon from '@mui/icons-material/Album';
+import { useUser } from "@auth0/nextjs-auth0/client";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import MenuIcon from "@mui/icons-material/Menu";
+import AlbumIcon from "@mui/icons-material/Album";
 
 const ResponsiveAppBar = () => {
   const { user, error, isLoading } = useUser();
@@ -40,10 +40,10 @@ const ResponsiveAppBar = () => {
   };
 
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#512da8' }}>
+    <AppBar position="static" sx={{ backgroundColor: "#512da8" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AlbumIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <AlbumIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
@@ -51,18 +51,18 @@ const ResponsiveAppBar = () => {
             href="/"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none'
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none"
             }}
           >
             LE DISQUAIRE
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -77,18 +77,18 @@ const ResponsiveAppBar = () => {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left'
+                vertical: "bottom",
+                horizontal: "left"
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left'
+                vertical: "top",
+                horizontal: "left"
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' }
+                display: { xs: "block", md: "none" }
               }}
             >
               [
@@ -98,7 +98,7 @@ const ResponsiveAppBar = () => {
               ]
             </Menu>
           </Box>
-          <AlbumIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <AlbumIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
           <Typography
             variant="h5"
             noWrap
@@ -106,18 +106,18 @@ const ResponsiveAppBar = () => {
             href="/"
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none'
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none"
             }}
           >
             LE DISQUAIRE
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {[
               <Link key="collection" href="/collection">
                 Collection
@@ -134,17 +134,17 @@ const ResponsiveAppBar = () => {
                   </IconButton>
                 </Tooltip>
                 <Menu
-                  sx={{ mt: '45px' }}
+                  sx={{ mt: "45px" }}
                   id="menu-appbar"
                   anchorEl={anchorElUser}
                   anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right'
+                    vertical: "top",
+                    horizontal: "right"
                   }}
                   keepMounted
                   transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right'
+                    vertical: "top",
+                    horizontal: "right"
                   }}
                   open={Boolean(anchorElUser)}
                   onClose={handleCloseUserMenu}
